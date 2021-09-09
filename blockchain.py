302ae3c1c81e094516094237878d33907eb9184a
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-09-09 20:16:52 -0300

    define a PoW method

--- blockchain.py
+++ blockchain.py
@@ -10,7 +10,7 @@ from urllib.parse import urlparse
 class Blockchain(object):
     difficulty_target = "0000"
 
-    def hash_block(selfself, block):
+    def hash_block(self, block):
         block_encoded = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_encoded).hexdigest()
 
@@ -21,3 +21,15 @@ class Blockchain(object):
         genesis_hash = self.hash_block("genesis_block")
 
         self.append_block(hash_of_previous_block = genesis_hash, nonce = self.proof_of_work(0, genesis_hash, []))
+
+    # use PoW to find the nonce for the current block
+    def proof_of_work(self, index, hash_of_previous_block, transactions):
+        # try with nonce = 0
+        nonce = 0
+
+        #try hashing the nonce together with the hash of the preivous block untils is valid
+        while self.valid_proof(index, hash_of_previous_block, transactions, nonce) is False:
+            nonce += 1
+
+        return nonce
+
